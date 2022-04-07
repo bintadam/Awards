50a9ffc41e1ae7a9aa5c0af2284d649df23730ec
zakiya adam <zakiyaadan12@gmail.com>
2022-04-07 14:13:14 +0300

    added class projects

--- trophies/models.py
+++ trophies/models.py
@@ -3,6 +3,32 @@ from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class Projects(models.Model):
+    name = models.CharField(max_length=100)
+    image = CloudinaryField('image')
+    design = models.IntegerField(default=0)
+    usability = models.IntegerField(default=0)
+    content = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=320)
+    link = models.URLField(max_length=60)
+    date = models.DateField(auto_now=True)
+    screen_one = CloudinaryField('image')
+    screen_two = CloudinaryField('image')
+
+    class Meta:
+        ordering = ['-name']
+
+    def __str__(self) -> str:
+        return self.name
+
+    @classmethod
+    def search_project(cls,word):
+        searched = cls.objects.filter(name__icontains = word)
+        return searched
+
+
 class Profile(models.Model):
     image = CloudinaryField('image')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
