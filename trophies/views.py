a7a0bd347c7c1307881e2c5c6ebc593d615b658d
zakiya adam <zakiyaadan12@gmail.com>
2022-04-07 14:05:14 +0300

    created function userprofile

--- trophies/views.py
+++ trophies/views.py
@@ -7,6 +7,31 @@ def homePage(request):
     return render(request, 'home.html')
 
 
+def userProfile(request):
+    current_user = request.user
+    try:
+        wasifu = Profile.objects.filter(user=current_user)[
+            0:1
+        ]  # wasifu is the same as profile
+        user_projects = Projects.objects.filter(user=current_user)
+    except Exception as e:
+        raise Http404()
+    if request.method == "POST":
+        form = UpdateForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+        return redirect("user_profile")
+    else:
+        form = UpdateForm()
+    return render(
+        request,
+        "user_profile.html",
+        {"form": form, "profile": wasifu, "projects": user_projects},
+    )    
+
+
 def post(request):
     current_user = request.user
     if request.method == "POST":
