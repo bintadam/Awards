f511c870cd8412377440564d6e3d350ff7cfe796
zakiya adam <zakiyaadan12@gmail.com>
2022-04-07 14:26:21 +0300

    created rate form

--- trophies/forms.py
+++ trophies/forms.py
@@ -4,3 +4,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Projects
         exclude = ['user','design','usability','content']
+
+
+class RateForm(forms.ModelForm):
+    class Meta:
+        model = Votes
+        exclude = ['user','project']        
