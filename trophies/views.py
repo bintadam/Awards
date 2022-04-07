62f444705002f6faf6c0d12b2c9ec595673116e8
zakiya adam <zakiyaadan12@gmail.com>
2022-04-07 14:00:13 +0300

    view function post

--- trophies/views.py
+++ trophies/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def homePage(request):
     return render(request, 'home.html')
+
+
+def post(request):
+    current_user = request.user
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+        return redirect("home_page")
+    else:
+        form = PostForm()
+    return render(request, "post.html", {"form": form})
