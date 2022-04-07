b89e453c5a8b27ce5f1850db5d87d9e5a641a999
zakiya adam <zakiyaadan12@gmail.com>
2022-04-07 14:45:43 +0300

    imported login

--- trophies/views.py
+++ trophies/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .forms import PostForm,  CommentForm, RateForm, UpdateForm
 from .models import Projects, Votes, Comments, Profile
 from django.contrib.auth.models import User
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpResponseRedirect
 from rest_framework.response import Response
 from rest_framework.views import APIView
