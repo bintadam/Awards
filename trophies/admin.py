3048b596210014f8b47a1954225144367e90f0d3
zakiya adam <zakiyaadan12@gmail.com>
2022-04-07 14:52:24 +0300

    added admin

--- trophies/admin.py
+++ trophies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile, Comments, Votes, Projects
+
 
 # Register your models here.
+admin.site.register(Projects)
+admin.site.register(Profile)
+admin.site.register(Comments)
+admin.site.register(Votes)
