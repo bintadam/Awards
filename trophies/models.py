9f1cf55fe858ce85251a2a5891b0b080d9201b71
zakiya adam <zakiyaadan12@gmail.com>
2022-04-07 14:06:24 +0300

    added model class profile

--- trophies/models.py
+++ trophies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    image = CloudinaryField('image')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['-image']
